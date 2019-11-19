e1016dec22a72703e9bd2843c2efee29476f475d
drewdzzz <Andrey22102001@gmail.com>
2019-11-19 22:12:08 +0300

    Added err codes to differentiate functions

--- differ.cpp
+++ differ.cpp
@@ -11,23 +11,13 @@ int main ()
     {
         abort ();
     }
-    
-    /*differ.draw ((char*)"open");
-    double result = 0;
-    differ.calculate (differ.head, result);
-    printf ("result: %lf\n", result);
-
-    differ.write_example (stdout);
-    $p;
-    differ.simplify (differ.head);
-    differ.draw ((char*)"open");
-    printf ("\n");
-    differ.write_example (stdout);
-    printf ("\n");*/
 
     CalcTree differed;
     delete differed.head;
-    differed.head = diff_funcs::differentiate (differ.head);
+    DFE::ERR ERR_CODE = DFE::OK;
+    differed.head = diff_funcs::differentiate (differ.head, ERR_CODE);
+    if (ERR_CODE != DFE::OK)
+        printf ("OT OK\n");
     differ.draw ("open");
     $p;
     differed.draw("open");
@@ -35,10 +25,6 @@ int main ()
     differed.simplify(differed.head);
     differed.draw("open");
     $p;
-    /*CalcTree differed_twice;
-    delete differed_twice.head;
-    differed_twice.head = diff_funcs::differentiate (differed.head);
-    differed_twice.draw("open");*/
 
     return 0;
 }
--- differ_rules.hpp
+++ differ_rules.hpp
@@ -5,48 +5,69 @@
 #include <math.h>
 #include "bin-tree.hpp"
 
+namespace DFE
+{
+    enum ERR
+    {
+        OK,
+        UNKNOWN_OP,
+        UNKNOWN_UN_FUNC,
+        NOT_DIFFER
+    };
+};
+
 struct diff_funcs 
 {
-    static CalcTree::Node_t* differentiate (CalcTree::Node_t *node)
+    static CalcTree::Node_t* differentiate (CalcTree::Node_t *node, DFE::ERR &err_code)
     {
+        err_code = DFE::OK;
+
         if (node -> data.variable)
         {
             CalcTree::Node_t *new_node = new CalcTree::Node_t;
             new_node -> data.value = 1;
             return new_node;
         }
-        if ( CalcTree::is_leaf (node) && ! node -> data.variable )
+        else if ( CalcTree::is_leaf (node) && ! node -> data.variable )
         {
             CalcTree::Node_t *new_node = new CalcTree::Node_t;
             new_node -> data.value = 0;
             return new_node;   
         }
-        if ( node -> data.op )
+        else if ( node -> data.op )
         {
-            return operator_diff (node);
+            return operator_diff (node, err_code);
         }
-        if ( node -> data.un_func )
+        else if ( node -> data.un_func )
         {
-            return unary_function_diff (node);
+            return unary_function_diff (node, err_code);
         }
+        else 
+        {
+            err_code = DFE::NOT_DIFFER;
+        }
+
 
     }
 
-    static CalcTree::Node_t* operator_diff (CalcTree::Node_t *node)
+    static CalcTree::Node_t* operator_diff (CalcTree::Node_t *node, DFE::ERR &err_code)
     {
         CalcTree::Node_t *new_node = new CalcTree::Node_t;
+
+        err_code = DFE::OK;
+
         switch (node -> data.op)
         {
             case '+':
                 new_node -> data.op = '+';
-                new_node -> left  = differentiate ( node -> left  );
-                new_node -> right = differentiate ( node -> right );
+                new_node -> left  = differentiate ( node -> left, err_code  );
+                new_node -> right = differentiate ( node -> right, err_code );
                 break;
 
             case '-':
                 new_node -> data.op = '-';
-                new_node -> left  = differentiate ( node -> left  );
-                new_node -> right = differentiate ( node -> right );
+                new_node -> left  = differentiate ( node -> left, err_code  );
+                new_node -> right = differentiate ( node -> right, err_code );
                 break;
 
             case '*':
@@ -56,10 +77,10 @@ struct diff_funcs
                 new_node -> right = new CalcTree::Node_t;
                     new_node -> right -> data.op = '*';
 
-                new_node -> left -> left = differentiate (node -> left);
+                new_node -> left -> left = differentiate (node -> left, err_code);
                 new_node -> left -> right = new CalcTree::Node_t ( *(node -> right) );
 
-                new_node -> right -> right = differentiate (node -> right);
+                new_node -> right -> right = differentiate (node -> right, err_code);
                 new_node -> right -> left = new CalcTree::Node_t ( *(node -> left) );
                 break;
 
@@ -81,10 +102,10 @@ struct diff_funcs
                 new_node -> left -> left = new CalcTree::Node_t;
                     new_node -> left -> left -> data.op = '*';
 
-                new_node -> left -> right -> right = differentiate (node -> right);
+                new_node -> left -> right -> right = differentiate (node -> right, err_code);
                 new_node -> left -> right -> left  = new CalcTree::Node_t ( *(node -> left) );
 
-                new_node -> left -> left -> left  = differentiate (node -> left);
+                new_node -> left -> left -> left  = differentiate (node -> left,   err_code);
                 new_node -> left -> left -> right = new CalcTree::Node_t ( *(node -> right) );
                 break;
 
@@ -105,31 +126,36 @@ struct diff_funcs
                     left_part -> right -> data.un_func = get_un_function_code (ln);     
                 left_part -> right -> right = new CalcTree::Node_t ( *(node -> left) );
 
-                new_node -> left = differentiate (left_part);
+                new_node -> left = differentiate (left_part, err_code);
 
                 CalcTree::free_tree (left_part);
 
                 free (ln);
                 break;
             }
-            default: delete new_node; new_node = nullptr; exit(228); break;
+            default: 
+                delete new_node; 
+                new_node = nullptr; 
+                err_code = DFE::UNKNOWN_OP; 
+                break;
         }
         return new_node;
     }
 
-    static CalcTree::Node_t* unary_function_diff (CalcTree::Node_t *node)
+    static CalcTree::Node_t* unary_function_diff (CalcTree::Node_t *node, DFE::ERR &err_code)
     {
         CalcTree::Node_t *new_node = new CalcTree::Node_t;
         if ( is_this_un_func (node -> data.un_func, "ln") )
         {
             new_node -> data.op = '/';
-            new_node -> left  = differentiate (node -> right);
+            new_node -> left  = differentiate (node -> right, err_code);
             new_node -> right = new CalcTree::Node_t ( *(node -> right) ); 
         }
         else
-        {    
-            exit(1488);
-        }
+        {
+            err_code = DFE::UNKNOWN_UN_FUNC;
+        }       
+
         return new_node;
     }
 
